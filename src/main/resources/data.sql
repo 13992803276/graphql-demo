ec31e9c0f95c2f20bd18ed1a8b7203aecf9350bf
lexu <377654325@qq.com>
2023-04-13 21:09:15 +0800

    refact sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,11 @@
-insert into t_school (name, ranking) values ('1班', 1);
-insert into t_school (name, ranking) values ('2班', 2);
+insert into t_school (name, ranking)
+values ('1班', 1);
+insert into t_school (name, ranking)
+values ('2班', 2);
 
-insert into t_student (name, age, class_id) values ('张三', 18, 1);
-insert into t_student (name, age, class_id) values ('李四', 18, 1);
-insert into t_student (name, age, class_id) values ('王五', 18, 2);
+insert into t_student (name, age, class_id)
+values ('张三', 18, 1);
+insert into t_student (name, age, class_id)
+values ('李四', 18, 1);
+insert into t_student (name, age, class_id)
+values ('王五', 18, 2);
